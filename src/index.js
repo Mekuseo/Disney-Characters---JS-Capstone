60395f2403ab4463c5e2dce1adca84e92cd13baf
Ghulam Subbhani <gsmalik030@gmail.com>
2023-02-10 14:25:13 +0200

    Final changes

--- src/index.js
+++ src/index.js
@@ -2,13 +2,12 @@ import './style.css';
 // eslint-disable-next-line import/no-cycle, import/named
 import { popup } from './popup.js';
 import { getData, getClicks } from './api.js';
-import { itemTotal } from './itemsTotal.js';
 
 const apiDisplay = document.querySelector('.api-display');
 
 const createCard = async () => {
+  let clicks = await getClicks();
   const images = await getData();
-  const clicks = await getClicks();
   images.forEach((image, index) => {
     const card = document.createElement('div');
     card.classList.add('card');
@@ -18,44 +17,38 @@ const createCard = async () => {
         <h2>${image.name}</h2>
         <i class="fa-solid fa-heart heart" data-id="${index}"></i>
       </div>
-      <p class='likes-count'>${clicks[index].likes} likes</p>
+      <p class='likes-count'>${clicks[index].likes} Likes</p>
       <button class='btn' data-id="${index}">Comments</button>
+      <button>Reservations</button>
       `;
     apiDisplay.appendChild(card);
   });
 
   const likeButton = document.querySelectorAll('.heart');
 
-  likeButton.forEach((e) => {
+  likeButton.forEach((e, index) => {
     e.addEventListener('click', async () => {
       e.classList.add('active');
       await fetch(
-        'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/UfLyVv1lq0qsaVGik4HQ/likes', {
+        'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/nkjqDKDvMMwAFsU6RQcX/likes', {
           method: 'POST',
           body: JSON.stringify({
-            item_id: `item${e.getAttribute('data-id')}`,
+            item_id: `item${index}`,
           }),
           headers: {
             'Content-type': 'application/json; charset=UTF-8',
           },
         },
       );
-
-      // Make another API call to get the updated likes count
-      const updatedClicks = await getClicks();
-      clicks[e.getAttribute('data-id')].likes = updatedClicks[e.getAttribute('data-id')].likes;
-
+      clicks = await getClicks();
       const updatedLikeCount = document.querySelectorAll('.likes-count');
-      updatedLikeCount[e.getAttribute('data-id')].innerHTML = `${clicks[e.getAttribute('data-id')].likes} Likes`;
+      updatedLikeCount[index].innerHTML = `${clicks[index].likes} Likes`;
     });
   });
-
   return document.querySelectorAll('.btn');
 };
-
 createCard();
 popup();
-itemTotal();
 
 // eslint-disable-next-line import/prefer-default-export
 export { createCard };
