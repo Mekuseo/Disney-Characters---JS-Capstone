491b8f5af48375853d01f04f776bbaa1f2387391
Chukwuemeka Kanu <mekuseo@gmail.com>
2023-02-10 10:49:04 +0100

    count update

---
+++ src/itemsTotal.js
@@ -0,0 +1,10 @@
+import { getData } from './api.js';
+
+const itemTotal = async () =>{
+  const total = await getData();
+  const items = document.querySelector('.items');
+  items.textContent = total.length;
+};
+
+// eslint-disable-next-line import/prefer-default-export
+export { itemTotal };
